21c3517f30a1709ac25068ef55ce9394394de058
Eryk Fryderyk <grzeniu098@gmail.com>
2021-09-10 00:05:25 +0200

    create playlist elemets with artists info

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -12,6 +12,76 @@
             </button>
           </div>
       </div>
+      <div class="playlist-box">
+          <ul class="playlist-box__list">
+              <li class="playlist-item">
+                    <img class="playlist-item__image" src="../assets/received_909123655824827.jpeg" alt="">
+                    <div class="playlist-item__song">
+                        <h3 class="playlist-item__song-name">Polska</h3>
+                        <h4 class="playlist-item__artist">Kazik</h4>
+                    </div>
+                    <div class="playlist-item__info">
+                        <span class="playlist-item__time">5:24</span>
+                        <span class="plalist-item__rate">******</span>
+                    </div>
+              </li>
+              <li class="playlist-item">
+                    <img class="playlist-item__image" src="../assets/received_909123655824827.jpeg" alt="">
+                    <div class="playlist-item__song">
+                        <h3 class="playlist-item__song-name">Polska</h3>
+                        <h4 class="playlist-item__artist">Kazik</h4>
+                    </div>
+                    <div class="playlist-item__info">
+                        <span class="playlist-item__time">5:24</span>
+                        <span class="plalist-item__rate">******</span>
+                    </div>
+              </li>
+              <li class="playlist-item">
+                    <img class="playlist-item__image" src="../assets/received_909123655824827.jpeg" alt="">
+                    <div class="playlist-item__song">
+                        <h3 class="playlist-item__song-name">Polska</h3>
+                        <h4 class="playlist-item__artist">Kazik</h4>
+                    </div>
+                    <div class="playlist-item__info">
+                        <span class="playlist-item__time">5:24</span>
+                        <span class="plalist-item__rate">******</span>
+                    </div>
+              </li>
+              <li class="playlist-item">
+                    <img class="playlist-item__image" src="../assets/received_909123655824827.jpeg" alt="">
+                    <div class="playlist-item__song">
+                        <h3 class="playlist-item__song-name">Polska</h3>
+                        <h4 class="playlist-item__artist">Kazik</h4>
+                    </div>
+                    <div class="playlist-item__info">
+                        <span class="playlist-item__time">5:24</span>
+                        <span class="plalist-item__rate">******</span>
+                    </div>
+              </li>
+              <li class="playlist-item">
+                    <img class="playlist-item__image" src="../assets/received_909123655824827.jpeg" alt="">
+                    <div class="playlist-item__song">
+                        <h3 class="playlist-item__song-name">Polska</h3>
+                        <h4 class="playlist-item__artist">Kazik</h4>
+                    </div>
+                    <div class="playlist-item__info">
+                        <span class="playlist-item__time">5:24</span>
+                        <span class="plalist-item__rate">******</span>
+                    </div>
+              </li>
+              <li class="playlist-item">
+                    <img class="playlist-item__image" src="../assets/received_909123655824827.jpeg" alt="">
+                    <div class="playlist-item__song">
+                        <h3 class="playlist-item__song-name">Polska</h3>
+                        <h4 class="playlist-item__artist">Kazik</h4>
+                    </div>
+                    <div class="playlist-item__info">
+                        <span class="playlist-item__time">5:24</span>
+                        <span class="plalist-item__rate">******</span>
+                    </div>
+              </li>
+          </ul>
+      </div>
   </div>
 </template>
 
@@ -30,12 +100,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.playlist{
+    background-color: #282F46;
+}
 .header{
-margin: 20px 0;
+padding: 20px 0;
 text-align: center;
+background-color: #202740;
+margin-bottom: 15px;
 
     &__title{
-        font-size: 30px;
+        font-size: 25px;
         color: #3DC864;
         font-family:'Arial Narrow', Arial, sans-serif;    
         text-transform: uppercase;    
@@ -51,9 +126,9 @@ text-align: center;
     align-items: center;
 
     &__input{
-        height: 50px;
+        height: 45px;
         width: 100%;
-        font-size: 30px;
+        font-size: 20px;
         letter-spacing: 1px;
         padding-left: 10px;
         color: #fff;
@@ -73,7 +148,7 @@ text-align: center;
         border: none;
         border-bottom: solid 2px #3DC864;
         border-top: solid 2px #3DC864;
-        height: 50px;
+        height: 45px;
         width: 50px;
         cursor: pointer;
         background-color: transparent;
@@ -81,8 +156,58 @@ text-align: center;
 
         svg{
             width: 100%;
-            height: 40px;
+            height: 30px;
         }
     }
 }
+.playlist-box{
+    margin: 0 20px;
+}
+.playlist-item{
+    display: flex;
+    align-items: center;
+
+    &__image{
+        height: 75px;
+        border-radius: 20px;
+    }
+
+    &__song{
+        margin-left: 10px;
+        width: 100%;
+        height: 100px;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        line-height: 1.5;
+    }
+
+    &__song-name{
+        font-family: 'Franklin Gothic', 'Arial Narrow', Arial, sans-serif;
+        font-size: 22px;
+    }
+
+    &__artist{
+        font-family: 'Franklin Gothic', 'Arial Narrow', Arial, sans-serif;
+        font-size: 18px;
+    }
+
+    &__info{
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        line-height: 1.5;
+        text-align: right;
+    }
+
+    &__time{
+        font-size: 20px;
+        font-family: 'Franklin Gothic';
+    }
+    &__rate{
+        font-size: 20px;
+        font-family: 'Franklin Gothic';
+    }
+}
+
 </style>
