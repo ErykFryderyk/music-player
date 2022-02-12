dfcb01d5a23b93d450a2979ea1e1a01da24b1066
Eryk Fryderyk <grzeniu098@gmail.com>
2022-02-12 09:35:16 +0100

    add new component

---
+++ src/components/PartyPeaple.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="users-list">
+    <div class="header">
+      <h2 class="header__title">party peaple</h2>
+    </div>
+    <UsersList/>
+  </div>
+</template>
+
+<script>
+import UsersList from './items/UsersList.vue';
+export default {
+  name: 'PartyPeaple',
+  components: {
+    UsersList,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.party-peaple{}
+.header{
+    text-align: center;
+    padding: 20px 0px;
+    width: 100%;
+    top: 0;
+    position: fixed;
+    background-color: #282F46;
+    border-bottom: 1px solid #3dc86473;
+
+    &__title{
+        font-size: 25px;
+        color: #3DC864;
+        font-family:'Arial Narrow', Arial, sans-serif;
+        text-transform: uppercase;    
+        letter-spacing: 2px;
+        font-weight: lighter;
+    }
+}
+</style>
