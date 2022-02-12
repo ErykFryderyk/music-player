eebe59c5d6c0c61cbb47d2eed03674699768b1dc
Eryk Fryderyk <grzeniu098@gmail.com>
2022-02-12 09:35:31 +0100

    add new item

---
+++ src/components/items/UsersList.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="users">
+    <ul class="users__list" v-for="user in users" :key="user.id">
+      <li class="users__item">
+        <img class="users__avatar" src="../../assets/received_909123655824827.jpeg" alt="">
+        <p class="users__name">{{user.userName}}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'UsersList',
+ data() {
+   return{
+     users: [
+      {
+        id: Math.random(),
+        userName: 'Antonina Kobylarek',
+        avatarImage: '',
+      },
+      {
+        id: Math.random(),
+        userName: 'Antonina Kobylarek',
+        avatarImage: '',
+      },
+      {
+        id: Math.random(),
+        userName: 'Antonina Kobylarek',
+        avatarImage: '',
+      },
+      {
+        id: Math.random(),
+        userName: 'Antonina Kobylarek',
+        avatarImage: '',
+      },
+     ]
+   }
+ }
+}
+</script>
+
+<style lang='scss' scoped>
+.users{
+    margin-top: 70px;
+
+    &__item{
+        display:flex;
+        align-items: center;
+        padding: 10px;
+        padding-left:10px;
+
+    }
+    &__list{
+      .users__list:nth-child(odd){
+        background-color: rgba(0, 0, 0, 0.08);
+      }
+    }
+
+    &__avatar{
+        width: 70px;
+        height: 70px;
+        border-radius: 50%;
+    }
+    &__name{
+        margin-left: 20px;
+        font-size: 22px;
+        font-family: 'Franklin Gothic', 'Arial Narrow', Arial, sans-serif;
+        letter-spacing: 1px;
+    }
+}
+</style>
