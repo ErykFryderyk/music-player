e911e5bf9eb863306e5d2e9e3eb171c87fc26300
Eryk Fryderyk <grzeniu098@gmail.com>
2022-02-15 22:47:31 +0100

    create new item

---
+++ src/components/items/LikeBtn.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="rate-btn rate-btn--like">
+    <font-awesome-icon icon="phone" />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'LikeBtn',
+  data() {
+    return{
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.rate-btn{
+  background: transparent;
+  border:none;
+  text-align: center;
+  line-height: 0.5;
+  cursor: pointer;
+  text-align: center;
+  font-size: 16px;
+  border-radius: 50px;
+  padding: 5px;
+
+  &--like{
+      border: 2px solid #127012;
+      color: #127012;
+  }
+  &--hate{
+      border: 2px solid red;
+      color: red;
+  }
+}
+</style>
