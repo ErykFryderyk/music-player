2bc55185bbfe5e0fd2ec5333e1ad72bd95f62255
Eryk Fryderyk <grzeniu098@gmail.com>
2022-02-18 22:45:40 +0100

    add font-awesome to project

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faPhone);
+library.add(faHeart);
+library.add(faThumbsDown);
 
 createApp(App)
 .component("font-awesome-icon", FontAwesomeIcon)
